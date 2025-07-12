de42f6aaf8554ef7f34e7c5c591b2c04f4423418
HarshExcollo <your-email@example.com>
2025-07-12 19:52:02 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enable static export for Firebase Hosting
-  output: 'export',
-  trailingSlash: true,
+  // trailingSlash: true,
   
 
   
